14d3f90fae4f8f3ca9a68b0b52c87ceeb2c5f358
imochoa <18612100+imochoa@users.noreply.github.com>
2024-10-09 15:09:21 +0200

    got treefmt working with 1 system

--- flake.nix
+++ flake.nix
@@ -3,18 +3,48 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # flake-root = {
+    #   url = "github:srid/flake-root";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs =
     inputs@{ self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
+      # systems = [ ${system} ];
       pkgs = import nixpkgs { inherit system; };
       pname = "fmtapp";
+      # Small tool to iterate over each systems
+      # eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+      # Eval the treefmt modules from ./treefmt.nix
+      # treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+      treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
     in
     {
+      # Inside outputs' `flake-parts.lib.mkFlake` 
+      # imports = [
+      #   inputs.treefmt-nix.flakeModule
+      #   inputs.flake-root.flakeModule
+      # ];
 
-      devShells.${system}.default = pkgs.mkShell { packages = [ ]; };
+      # for `nix fmt`
+      # formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+      formatter.${system} = treefmtEval.config.build.wrapper;
+      # for `nix flake check`
+      # checks = eachSystem (pkgs: { formatting = treefmtEval.${pkgs.system}.config.build.check self; });
+      # checks = eachSystem (pkgs: { formatting = treefmtEval.${pkgs.system}.config.build.check self; });
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.treefmt2
+        ];
+      };
       packages = {
         ${system} = {
           default = pkgs.writeShellApplication {
